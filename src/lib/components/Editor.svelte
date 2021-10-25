7549d238bff4a5bf66fd63b131c86cef47db52bc
trainingmontage <nemo.omen@gmail.com>
2021-10-25 17:35:42 -0500

    Editor changes

--- src/lib/components/Editor.svelte
+++ src/lib/components/Editor.svelte
@@ -1,14 +1,41 @@
 <script>
 	import Toolbar from '$lib/components/Toolbar.svelte';
 	import Header from './Header.svelte';
+	function logKeypress(event) {
+		if (event.key === '[') {
+			console.log('left bracket!!');
+		} else if (event.key === ']') {
+			console.log('right bracket!!');
+		}
+	}
+
+	function handleSelection(event) {
+		console.log(document.getSelection());
+	}
 </script>
 
-<section id="editor">
+<section id="editor-pane">
 	<Toolbar />
+	<textarea class="editor" on:keypress={logKeypress} on:select={handleSelection} />
 </section>
 
 <style lang="scss">
-	#editor {
+	.editor {
+		font-size: 1rem;
+		line-height: 1.5;
+		font-family: 'Courier New', Courier, monospace;
+		min-height: 90%;
+		min-width: 100%;
+		background-color: var(--primary-bg);
+		color: var(--primary-fg);
+		border: none;
+		padding: 1rem;
+		resize: none;
 		overflow-y: scroll;
+	}
+	.editor:focus {
+		background-color: rgba(var(--primary-fg), 0.1);
+		outline: 3px solid rgba(var(--link-rgb), 0.3);
+		outline-offset: -3px;
 	}
 </style>
